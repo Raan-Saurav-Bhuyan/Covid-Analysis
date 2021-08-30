52eeb229afebada54eb4a31209326d9c092e177e
Raan Saurav Bhuyan <74211378+Raan-Saurav-Bhuyan@users.noreply.github.com>
2021-08-30 12:31:10 +0530

    Commit 1: EXECUTE COMMANDS
    1. Command: gradle.bat init
       Added + modified auto-generated files:
          (i) gradle-wrapper.properties
             (a) Optimized: Gradle v7.2 support to Wrapper.
          (ii) /app/build.gradle
             (a) Optimized: Commented out unit-test plug-in.
    
       Newly added auto-generated file:
          (i) settings.gradle
    
       Newly added source files:
          (i) main.cpp,
             (a) Optimized: Design language.
          (ii) app_test.cpp
             (a) Optimized: Sample assert() function template.
    
    2. Command: .\gradlew build
       Modified auto-generated file:
          (i) git-attributes
    
       Added + modified auto-generated file:
          (i) git-ignore
             (a) Optimized: Gradle, Build (debug + release) and VS files tracking Removed.
    
    Signed-off-by: Raan Saurav Bhuyan <74211378+Raan-Saurav-Bhuyan@users.noreply.github.com>

---
+++ app/src/main/cpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << endl << "\n=========================================================================";
+	cout << "=======================================================================" << endl;
+
+	cout << "\t\t\t\t||\t\t    Covid-Analysis C++ Application\t\t\t||" << endl;
+	cout << "\t\t\t\t||\tEstimating the COVID-19 Evolution Using the Logistic Model\t||" << endl;
+	cout << "\t\t\t\t||\t\t   Raan Saurav Bhuyan, August, 2021\t\t\t||" << endl;
+
+	cout << "=========================================================================";
+	cout << "=======================================================================\n" << endl;
+
+	return 0;
+}
