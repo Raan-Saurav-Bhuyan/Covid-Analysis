569556c159af1ddecb36fa25b451c5a976c21b2a
Raan Saurav Bhuyan <74211378+Raan-Saurav-Bhuyan@users.noreply.github.com>
2021-09-05 23:44:31 +0530

    Commit 1: TEST CLASSES UNDER SOURCE NAMESPACES
    1. Namespace: cppMain
       (i) Class 1: testWhatif
          (a) (Static) Method 1: pass_WhatifAtBegin(): Tests the default input of the 'whatif' string variable under favourable conditions.
          (b) (Static) Method 2: ret_Value(): Returns the updated value if the condition matches or original value if the condition mismatches.
          (c) Method 3: pass_WhatifAtLast(): Returns integer 1 if the condition matches as an indicator that the 'whatif' string variable should pass the default input.
    
    2. Namespace: cppManual
       (i) Class 1: testcountryName
          (a) (Static) Method 1: test_Country(): Tests if the blank value checker is working as intended or not.
          (b) (Static) Method 2: check_CountryName(): Tests whether the country's name is capitalised for use in title bar or not.
    
    Signed-off-by: Raan Saurav Bhuyan <74211378+Raan-Saurav-Bhuyan@users.noreply.github.com>

---
+++ app/src/main/test-headers/test-main.h
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+namespace cppMain {
+
+	class testWhatif {
+
+	public:
+
+		static string pass;
+
+		static void pass_WhatifAtBegin(string value) {
+
+			if (value.length() == 0)
+				value = "y";
+
+			pass = value;
+		}
+
+		static string ret_Value() {
+
+			return pass;
+		}
+
+		int pass_WhatifAtLast(string value) {
+
+			if (value.length() == 0 || value == "n")
+				return 1;
+		}
+
+	private:
+		;
+	};
+}
+
+namespace cppManual {
+
+	class testcountryName {
+
+	public:
+
+		static int test_Country(string name) {
+
+			if (name.length() == 0)
+				return 0;
+		}
+
+		static string check_CountryName(string countryName) {
+
+			string duplicate = countryName;		/* Local variable to temporarily hold the				 *
+												 * string input of countryName.							 *
+												 */
+
+			//============================================================================================//
+			/**
+			 ** transform() function defined under the 'algorithm.h' header file						 *
+			 ** is used to change the 'duplicate' string into uppercase.								 *
+			 **/
+
+			transform(duplicate.begin(), duplicate.end(), duplicate.begin(), ::toupper);
+
+			return duplicate;
+			//===========================================================================================//
+		}
+
+	private:
+		;
+	};
+}
